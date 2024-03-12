a18ca4b2f006dbe1c7bce7135cd21e6605ced54a
Damian Banach <damian070799@wp.pl>
2024-03-12 16:23:35 +0100

    created model Film

--- filmy/models.py
+++ filmy/models.py
@@ -1,3 +1,12 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+    tytul = models.CharField(max_length=50)
+    opis = models.CharField(max_length=255)
+    producent = models.CharField(max_length=255)
+    rezyser = models.CharField(max_length=255)
+    premiera = models.DateTimeField(default=datetime.now, blank=True)
+    ocena = models.IntegerField(default=0)
