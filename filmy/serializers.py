e114d8df7cf1f84c90ac04851ed24e7ea634fb91
Damian Banach <damian070799@wp.pl>
2024-03-12 16:33:40 +0100

    created functions: create and update for Film Serializer

--- filmy/serializers.py
+++ filmy/serializers.py
@@ -14,3 +14,16 @@ class FilmSerializer(serializers.ModelSerializer):
             'rok',
             'ocena'
         ]
+
+    def create(self, validated_data):
+        return Film.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.tytul = validated_data.get('tytul', instance.tytul)
+        instance.opis = validated_data.get('opis', instance.opis)
+        instance.producent = validated_data.get('producent', instance.producent)
+        instance.rezyser = validated_data.get('rezyser', instance.rezyser)
+        instance.rok = validated_data.get('rok', instance.rok)
+        instance.ocena = validated_data.get('ocena', instance.ocena)
+        instance.save()
+        return instance
