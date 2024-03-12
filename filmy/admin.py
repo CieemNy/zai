2a74443540b3e5f001d55de9ada491c1b49f4aba
Damian Banach <damian070799@wp.pl>
2024-03-12 16:44:29 +0100

    created FilmAdmin

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Film
 
-# Register your models here.
+
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ['id', 'tytul', 'opis', 'producent', 'rezyser', 'rok', 'ocena']
+
+
+admin.site.register(Film, FilmAdmin)
+
