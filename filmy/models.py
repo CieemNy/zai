f96ab090c5ce0451db06de1a115ff9cab7ebf63b
Damian Banach <damian070799@wp.pl>
2024-03-12 16:26:46 +0100

    updated model film

--- filmy/models.py
+++ filmy/models.py
@@ -1,12 +1,15 @@
-from datetime import datetime
+
 
 from django.db import models
 
 
 class Film(models.Model):
     tytul = models.CharField(max_length=50)
-    opis = models.CharField(max_length=255)
+    opis = models.TextField(default="")
     producent = models.CharField(max_length=255)
     rezyser = models.CharField(max_length=255)
-    premiera = models.DateTimeField(default=datetime.now, blank=True)
+    rok = models.IntegerField(blank=False)
     ocena = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "{} ({})".format(self.tytul, str(self.rok))
