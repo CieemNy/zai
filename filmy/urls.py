9dddb456c59a32267d0de9d760a3c960ef08c92b
Damian Banach <damian070799@wp.pl>
2024-03-19 16:12:01 +0100

    added first views

--- filmy/urls.py
+++ filmy/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import *
 
 urlpatterns = [
-    path('wszystkie/', views.wszystkie),
-    path('szczegoly/', views.szczegoly),
+    path('filmlist/', views.ListFilm.as_view(), name="film-list"),
+    path('filmlist/<int:pk>/', views.RetrieveFilm.as_view(), name="film-retrieve"),
+    path('filmcreate/', views.CreateFilm.as_view(), name="film-create"),
 ]
