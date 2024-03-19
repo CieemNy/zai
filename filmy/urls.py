9f0963f1a829d86b33dd503df833266bb7b4ef76
Damian Banach <damian070799@wp.pl>
2024-03-19 16:24:39 +0100

    updated urls

--- filmy/urls.py
+++ filmy/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('filmlist/<int:pk>/', views.RetrieveFilm.as_view(), name="film-retrieve"),
     path('filmcreate/', views.CreateFilm.as_view(), name="film-create"),
     path('filmdelete/<int:pk>/', views.DestroyFilm.as_view(), name="film-destroy"),
+    path('filmupdate/<int:pk>/', views.UpdateFilm.as_view(), name="film-update"),
 ]
