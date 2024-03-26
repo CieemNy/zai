a94b844ab3736318e033b485dd1db7d14af5edb0
Damian Banach <damian070799@wp.pl>
2024-03-26 16:43:06 +0100

    added urls

--- filmy/urls.py
+++ filmy/urls.py
@@ -8,6 +8,13 @@ urlpatterns = [
     path('filmcreate/', views.CreateFilm.as_view(), name="film-create"),
     path('filmdelete/<int:pk>/', views.DestroyFilm.as_view(), name="film-destroy"),
     path('filmupdate/<int:pk>/', views.UpdateFilm.as_view(), name="film-update"),
-    path('userlist/', UserList.as_view(), name='UserList'),
-    path('usercreatelist/', UserCreateList.as_view(), name='UserCreateList')
+    path('extrainfo/', ExtraInfoCreateList.as_view(), name='ExtraInfoCreateList'),
+    path('extrainfo/<int:pk>/', ExtraInfoRetrieveUpdateDestroy.as_view(), name='ExtraInfoRetrieveUpdateDestroy'),
+    path('ocena/', OcenaCreateList.as_view(), name='OcenaCreateList'),
+    path('ocena/<int:pk>/', OcenaRetrieveUpdateDestroy.as_view(), name='OcenaRetrieveUpdateDestroy'),
+    path('aktor/', AktorCreateList.as_view(), name='AktorCreateList'),
+    path('aktor/<int:pk>/', AktorRetrieveUpdateDestroy.as_view(), name='AktorRetrieveUpdateDestroy'),
+    path('userlist/', UserCreateList.as_view(), name='UserList'),
+    path('usercreatelist/', UserCreateList.as_view(), name='UserCreateList'),
+    path('user/<int:pk>/', UserRetrieveUpdateDestroy.as_view(), name='UserRetrieveUpdateDestroy')
 ]
