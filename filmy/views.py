555fc14e47b0d7fae77b4d750fc9268685b23eb0
Damian Banach <damian070799@wp.pl>
2024-03-26 16:59:23 +0100

    updated ListCreateFilm view

--- filmy/views.py
+++ filmy/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .serializers import *
@@ -5,23 +6,25 @@ from .permissions import IsOwnerOrReadOnly
 
 
 class ListCreateFilm(generics.ListAPIView):
-    queryset = Film.objects.all()
+    # queryset = Film.objects.all().order_by('-rok','tytul')
     serializer_class = FilmSerializer
-    name = "list-film"
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['id', 'tytul', 'rok', 'opis']
+    # search_fields = ['tytul', 'opis', 'rok']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    def get_queryset(self):
-        queryset = Film.objects.all().order_by('-rok', 'tytul')
-        tytul = self.request.query_params.get('tytul')
-        id = self.request.query_params.get('id')
-        if tytul is not None:
-            queryset = queryset.filter(tytul__startswith=tytul)
-        if id is not None:
-            queryset = queryset.filter(id__exact=id)
-        return queryset
+    # def get_queryset(self):
+    #     queryset = Film.objects.all().order_by('-rok', 'tytul')
+    #     tytul = self.request.query_params.get('tytul')
+    #     id = self.request.query_params.get('id')
+    #     if tytul is not None:
+    #         queryset = queryset.filter(tytul__startswith=tytul)
+    #     if id is not None:
+    #         queryset = queryset.filter(id__exact=id)
+    #     return queryset
 
 
 class RetrieveUpdateDestroyFilm(generics.RetrieveUpdateDestroyAPIView):
