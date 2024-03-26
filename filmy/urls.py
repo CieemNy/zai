d29efeb4add262c027eaf88d22c0f00ab90c8323
Damian Banach <damian070799@wp.pl>
2024-03-26 16:54:30 +0100

    updated views for model: Film

--- filmy/urls.py
+++ filmy/urls.py
@@ -3,11 +3,11 @@ from . import views
 from .views import *
 
 urlpatterns = [
-    path('filmlist/', views.ListFilm.as_view(), name="film-list"),
-    path('filmlist/<int:pk>/', views.RetrieveFilm.as_view(), name="film-retrieve"),
-    path('filmcreate/', views.CreateFilm.as_view(), name="film-create"),
-    path('filmdelete/<int:pk>/', views.DestroyFilm.as_view(), name="film-destroy"),
-    path('filmupdate/<int:pk>/', views.UpdateFilm.as_view(), name="film-update"),
+    path('filmlist/', views.ListCreateFilm.as_view(), name="film-list"),
+    path('filmlist/<int:pk>/', views.RetrieveUpdateDestroyFilm.as_view(), name="film-retrieve"),
+    path('filmcreate/', views.ListCreateFilm.as_view(), name="film-create"),
+    path('filmdelete/<int:pk>/', views.RetrieveUpdateDestroyFilm.as_view(), name="film-destroy"),
+    path('filmupdate/<int:pk>/', views.RetrieveUpdateDestroyFilm.as_view(), name="film-update"),
     path('extrainfo/', ExtraInfoCreateList.as_view(), name='ExtraInfoCreateList'),
     path('extrainfo/<int:pk>/', ExtraInfoRetrieveUpdateDestroy.as_view(), name='ExtraInfoRetrieveUpdateDestroy'),
     path('ocena/', OcenaCreateList.as_view(), name='OcenaCreateList'),
--- filmy/views.py
+++ filmy/views.py
@@ -1,38 +1,34 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .serializers import *
+from .permissions import IsOwnerOrReadOnly
 
 
-class ListFilm(generics.ListAPIView):
+class ListCreateFilm(generics.ListAPIView):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
     name = "list-film"
-
-
-class RetrieveFilm(generics.RetrieveAPIView):
-    queryset = Film.objects.all()
-    serializer_class = FilmSerializer
-    name = "retrieve-film"
-
-
-class CreateFilm(generics.ListCreateAPIView):
-    queryset = Film.objects.all()
-    serializer_class = FilmSerializer
-    name = "create-film"
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    def get_queryset(self):
+        queryset = Film.objects.all().order_by('-rok', 'tytul')
+        tytul = self.request.query_params.get('tytul')
+        id = self.request.query_params.get('id')
+        if tytul is not None:
+            queryset = queryset.filter(tytul__startswith=tytul)
+        if id is not None:
+            queryset = queryset.filter(id__exact=id)
+        return queryset
 
-class DestroyFilm(generics.DestroyAPIView):
+
+class RetrieveUpdateDestroyFilm(generics.RetrieveUpdateDestroyAPIView):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
-    name = "destroy-film"
-
-
-class UpdateFilm(generics.UpdateAPIView):
-    queryset = Film.objects.all()
-    serializer_class = FilmSerializer
-    name = "update-film"
+    name = "retrieve-film"
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 
 class UserCreateList(generics.ListCreateAPIView):
