ba0fdd29745dc6f5568bccbb532030ceb0093932
Damian Banach <damian070799@wp.pl>
2024-03-26 16:37:38 +0100

    added serializers for ExtraInfo, Ocena and Aktor

--- filmy/serializers.py
+++ filmy/serializers.py
@@ -1,9 +1,45 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Film
+from .models import *
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class UserSerializerShort(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "is_superuser", "email", "is_staff", "is_active"]
+
+
+class ExtraInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExtraInfo
+        fields = ['czas_trwania', 'gatunek', 'rezyser', 'filmy']
+
+
+class OcenaSerializer(serializers.ModelSerializer):
+    class Meta: model = Ocena
+
+    fields = ['recenzja', 'gwiazdki', 'film']
+
+
+class AktorSerializer(serializers.ModelSerializer):
+    filmy = serializers.SlugRelatedField(slug_field='tytul', queryset=Film.objects.all(), many=True)
+
+    class Meta:
+        model = Aktor
+
+    fields = ['id', 'imie', 'nazwisko', 'filmy']
 
 
 class FilmSerializer(serializers.ModelSerializer):
+    extrainfo = serializers.StringRelatedField()
+    ocena_set = OcenaSerializer(read_only=True, many=True)
+    aktor_set = serializers.StringRelatedField(read_only=True, many=True)
     class Meta:
         model = Film
         fields = [
@@ -31,15 +67,3 @@ class FilmSerializer(serializers.ModelSerializer):
 
     def destroy(self, instance):
         instance.delete()
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-
-
-class UserSerializerShort(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["username", "is_superuser", "email", "is_staff", "is_active"]
