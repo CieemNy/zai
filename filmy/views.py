cf2d111536a1a36b51715353d7cbd6dca18c444c
Damian Banach <damian070799@wp.pl>
2024-04-16 16:09:26 +0200

    updated user views

--- filmy/views.py
+++ filmy/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .serializers import *
 from .permissions import IsOwnerOrReadOnly
@@ -38,15 +38,51 @@ class RetrieveUpdateDestroyFilm(generics.RetrieveUpdateDestroyAPIView):
 
 
 class UserCreateList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializerShort
+
+    def create(self, request, *args, **kwargs):
+        user = self.request.user
+        if user.is_superuser:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response(data=None, status=status.HTTP_403_FORBIDDEN)
 
 
 class UserRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializerShort
+
+    def update(self, request, *args, **kwargs):
+        user = self.request.user
+        if user.is_superuser:
+            partial = kwargs.pop('partial', False)
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+
+            if getattr(instance, '_prefetched_objects_cache', None):
+                instance._prefetched_objects_cache = {}
+
+            return Response(serializer.data)
+        else:
+            return Response(data=None, status=status.HTTP_403_FORBIDDEN)
+
+    def destroy(self, request, *args, **kwargs):
+        user = self.request.user
+        if user.is_superuser:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
 
 
 class ExtraInfoCreateList(generics.ListCreateAPIView):
